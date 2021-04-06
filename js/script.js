58b1deb5b4e5295f0883cfd41d700edbbe294a14
vektor2020 <lexaro2011@gmail.com>
2021-04-07 00:03:34 +0300

    var03

--- js/script.js
+++ js/script.js
@@ -1,21 +1,28 @@
 var age = prompt('Введите свой возраст');
-var age100 = age % 100;
-if (age100 >= 5 && age100 <= 20) {
-    alert(age + ' лет');
-} else {
-var age10 = age100 % 10;
-switch (age10) {
-    case 1:
-        alert(age + ' год');
-        break;
+
+if (isNaN(age)){
+    alert('Введите год цыфрами');
     
-    case 2:
-    case 3:
-    case 4:
-        alert(age + ' года');
-        break;
-    
-    default:
+}else if (age < 0){ 
+    alert('Слишком мало')
+}else{
+    var age100 = age % 100;
+    if (age100 >= 5 && age100 <= 20) {
         alert(age + ' лет');
-        break;
-} }
+    } else {
+            var age10 = age100 % 10;
+            switch (age10) {
+                case 1:
+                    alert(age + ' год');
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                    alert(age + ' года');
+                    break;
+                default:
+                    alert(age + ' лет');
+                    break;
+            } 
+    }
+}
